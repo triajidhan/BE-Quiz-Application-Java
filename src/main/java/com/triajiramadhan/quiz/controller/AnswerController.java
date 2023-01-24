aa1a0423f2dbbe9f17708f1c1bc8576971e2d1e9
triajidhan <triajidhan@gmail.com>
2023-01-24 16:25:30 +0700

    add swagger in pom and add answer controller

--- src/main/java/com/triajiramadhan/quiz/controller/AnswerController.java
+++ src/main/java/com/triajiramadhan/quiz/controller/AnswerController.java
@@ -1,10 +1,38 @@
 package com.triajiramadhan.quiz.controller;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.triajiramadhan.quiz.dto.answer.AnswerUpdateReqDto;
+import com.triajiramadhan.quiz.dto.base.BaseDeleteResDto;
+import com.triajiramadhan.quiz.dto.base.BaseUpdateResDto;
+import com.triajiramadhan.quiz.service.AnswerService;
 
 @RestController
 @RequestMapping("answers")
 public class AnswerController {
 
+	@Autowired
+	private AnswerService answerService;
+	
+	@PutMapping
+	public ResponseEntity<BaseUpdateResDto> update(@Valid @RequestBody  final AnswerUpdateReqDto data) {
+		final BaseUpdateResDto updateRes = answerService.update(data);
+		return new ResponseEntity<>(updateRes, HttpStatus.OK);
+	}
+
+	@DeleteMapping("{id}")
+	public ResponseEntity<BaseDeleteResDto> delete(@PathVariable("id")  final String id) {
+		final BaseDeleteResDto deleteRes = answerService.deleteById(id);
+		return new ResponseEntity<>(deleteRes, HttpStatus.OK);
+	}
 }
